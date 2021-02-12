2ad948d689e73d384516ee438979708889920533
Devanthedev <77267537+DEVAN1220@users.noreply.github.com>
2021-02-12 12:36:41 +0530

    Add files via upload

---
+++ Loginjs.js
@@ -0,0 +1,16 @@
+function functio(){
+  
+  
+  var psw = document.getElementById("pw").value;
+  if (psw=="dev") {
+    window.location= "Logins.html"
+   
+   
+  }
+  else{
+    
+    window.location= "Loging.html"
+  }
+  
+  
+}
